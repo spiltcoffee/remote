bc9f88b3f914142e06b3942b6cbbc8cefe22326d
spiltcoffee <spiltcoffee@users.noreply.github.com>
2020-04-13 14:17:13 +1000

    added manifest and service-worker, is this now a PWA?!

--- src/main.js
+++ src/main.js
@@ -8,3 +8,16 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
 module.exports = {
   transpileDependencies: ["vuetify"],
   outputDir: "../../www/html/remote",
   publicPath: "",
+  configureWebpack: {
+    plugins: [
+      new WorkboxWebpackPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 100000,
+      }),
+    ],
+  },
 };
